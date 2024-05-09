c156c440b704c48a732e6d263070222d747382c6
kamilmccads <79862950+kamilmccads@users.noreply.github.com>
2024-05-09 16:20:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function generateUtm() {
     var source = document.getElementById('source').value;
     var medium = document.getElementById('medium').value;
     var campaign = document.getElementById('campaign').value;
+    var campaignId = document.getElementById('campaignId').value;
     var term = document.getElementById('term').value;
     var content = document.getElementById('content').value;
-    var utmId = document.getElementById('utmId').value;
 
     var url = new URL(baseUrl);
     if (source) url.searchParams.append('utm_source', source);
     if (medium) url.searchParams.append('utm_medium', medium);
     if (campaign) url.searchParams.append('utm_campaign', campaign);
+    if (campaignId) url.searchParams.append('utm_id', campaignId);
     if (term) url.searchParams.append('utm_term', term);
     if (content) url.searchParams.append('utm_content', content);
-    if (utmId) url.searchParams.append('utm_id', utmId);
 
     var displayUrl = document.getElementById('generatedUrl');
     displayUrl.textContent = url.href;
