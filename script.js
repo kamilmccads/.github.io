20182a575663baac4c5520ba447681b92a1ee5bf
kamilmccads <79862950+kamilmccads@users.noreply.github.com>
2024-06-04 15:27:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,7 @@ function generateUtm() {
     if (term) url.searchParams.append('utm_term', term);
     if (content) url.searchParams.append('utm_content', content);
 
-    // Replace %7B with { and %7D with } in the URL
-    var finalUrl = url.href.replace(/%7B/g, '{').replace(/%7D/g, '}');
-
-    // Handle specific placeholder replacements
-    finalUrl = finalUrl.replace(/{campaignId1}/g, '{campaignId}');
-    finalUrl = finalUrl.replace(/{keyword1}/g, '{keyword}');
-    finalUrl = finalUrl.replace(/{creative1}/g, '{creative}');
-
     // Create a clickable link in the generatedUrl div
     var displayUrl = document.getElementById('generatedUrl');
-    displayUrl.innerHTML = '<a href="' + finalUrl + '" target="_blank">' + finalUrl + '</a>';
+    displayUrl.innerHTML = '<a href="' + url.href + '" target="_blank">' + url.href + '</a>';
 }
