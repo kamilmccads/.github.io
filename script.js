d417f664584c4e8d2a8df7afaf642bd9cbe8a7c9
kamilmccads <79862950+kamilmccads@users.noreply.github.com>
2024-06-04 15:46:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ function generateUtm() {
     // Decode the URL to replace %7B with { and %7D with }
     var decodedUrl = decodeURIComponent(url.href);
 
+    // Replace specific encoded characters with the correct braces
+    var finalUrl = decodedUrl.replace(/%7B/g, '{').replace(/%7D/g, '}');
+
     // Create a clickable link in the generatedUrl div
     var displayUrl = document.getElementById('generatedUrl');
-    displayUrl.innerHTML = '<a href="' + decodedUrl + '" target="_blank">' + decodedUrl + '</a>';
+    displayUrl.innerHTML = '<a href="' + finalUrl + '" target="_blank">' + finalUrl + '</a>';
 }
